360f48280e500bf1309356d9f6b91db96eec651a
TabulaWeb <tabulaweb@mail.ru>
2020-08-20 10:49:55 +0300

    page musik_text

--- harmused/hurmused/urls.py
+++ harmused/hurmused/urls.py
@@ -6,5 +6,5 @@ app_name = 'musik'
 urlpatterns = [
  # post views
  path('', views.musik_list, name='musik_list'),
- path('<int:year>/<int:month>/<int:day>/<slug:musik>/', views.musik_detail, name='musik_detail'),
+ path('<int:year>/<int:month>/<int:day>/<slug:musiks>/', views.musik_detail, name='musik_detail'),
 ]
--- harmused/hurmused/views.py
+++ harmused/hurmused/views.py
@@ -6,6 +6,6 @@ def musik_list(request):
     musik_posts = Musik.published.all()
     return render(request, 'index.html', {'musik_posts': musik_posts})
 
-def musik_detail(request, year, month, day, musik):
-    musik = get_list_or_404(Musik, slug=musik, status='published', publish__year=year, publish__month=month, publish__day=day)
-    return render(request, 'musik_detail.html', {'musik': musik})
+def musik_detail(request, year, month, day, musiks):
+    musiks = get_list_or_404(Musik, slug=musiks, status='published', publish__year=year, publish__month=month, publish__day=day)
+    return render(request, 'musik_detail.html', {'musiks': musiks})
